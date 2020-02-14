49fb4c23e59b7c0b41d64e0b684997af68f7fce2
Alin Voinea <contact@avoinea.com>
2020-02-14 16:19:26 +0200

    Fix python syntax

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eionet_external',
-    version='1.0'
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
