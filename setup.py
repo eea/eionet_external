315949d0d791c4817d8520762de64792d14b471a
Alin Voinea <contact@avoinea.com>
2020-02-14 16:16:06 +0200

    Prepare new release

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eionet_external',
+    version='1.0'
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
